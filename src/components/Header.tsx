2d726a04d79d082697a68d75599406b5bd8496b4
shakkeerz <shakkeerpersonal@gmail.com>
2023-12-20 14:53:53 +0530

    mubile responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { BsBell,  } from "react-icons/bs"
+import { BsBell, } from "react-icons/bs"
 import { IoChevronDownSharp, } from "react-icons/io5"
 import { IoIosMenu } from "react-icons/io"
 
@@ -8,30 +8,32 @@ interface HeaderProps {
 
 export const Header: React.FC<HeaderProps> = ({ toggleMenu }) => {
     return (
-        <header className="flex justify-between items-center px-5  w-[80%]">
+        <header className="flex justify-center md:justify-between items-center md:px-5  w-[80%]">
 
             <img src="/imgs/logo.png" alt="logo" className=" w-[100px] md:w-[150px] " />
 
             {/* dropdown menu */}
-            <div className="flex  gap-4">
-                <div className="w-[250px] h-[50px] bg-white rounded-md flex gap-3 items-center justify-center">
+            <div className="flex gap-2 md:gap-4">
+                <div className="w-[150px] md:w-[250px] h-[50px] bg-white rounded-md flex gap-3 items-center justify-center">
                     <img src="/imgs/logo.png" alt="logo" className="rounded-full w-[40px] h-[40px]" />
-                    <h5 className="text-[15px]">XYZ Enterprice Pvt. Ltd</h5>
+                    <h5 className="text-[10px] md:text-[15px]">XYZ Enterprice Pvt. Ltd</h5>
                 </div>
                 <div className="w-[50px] h-[50px] flex items-center justify-center bg-white rounded-md cursor-pointer">
                     <IoChevronDownSharp />
                 </div>
             </div>
 
-{/*     position: absolute;
+            {/*     position: absolute;
     top: 200px;
     right: 100px;
     z-index: 99; */}
-            <button className="static lg:absolute top-[200px] right-[100px] z-50 group p-4 bg-primary">
-                <BsBell className="text-[20px] " />
-                <div className="w-4 h-4 rounded-full bg-red-500 text-[10px] group-hover:animate-pulse">3</div>
-            </button>
-            <button className="md:hidden" onClick={toggleMenu}><IoIosMenu /></button>
+            <div className="flex">
+                <button className="static lg:absolute top-[200px] right-[100px] z-50 group p-4 bg-primary">
+                    <BsBell className="text-[20px] " />
+                    <div className="w-4 h-4 rounded-full bg-red-500 text-[10px] group-hover:animate-pulse">3</div>
+                </button>
+                <button className="text-[50px] md:hidden" onClick={toggleMenu}><IoIosMenu /></button>
+            </div>
         </header>
     )
 }
